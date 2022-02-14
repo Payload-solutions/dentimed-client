a265e8954b7da4c1b37485fbec96c25c81f62301
Arturo0911 <anegreiross@outlook.com>
2022-02-14 17:04:15 -0500

    added new information about the photos from another website

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -22,6 +22,40 @@
         <div class="card">
           <img
             class="card-img-top"
+            src="../../temp/images/deliver.jpg"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <img
+            class="card-img-top"
+            src="../../temp/images/001.jpg"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <img
+            class="card-img-top"
             src="../assets/box.jpg"
             alt="Card image cap"
           />
