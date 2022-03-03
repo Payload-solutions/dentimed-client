6758bc0de93e7ece37272c860e7229a1b435c2f0
Arturo0911 <anegreiross@outlook.com>
2022-03-02 23:34:48 -0500

    ok

---
+++ index.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+import requests
+
+
+class Exploit:
+
+    def __init__(self):
+        pass
+
+
+
+def main():
+    pass
--- src/views/ortodoncia/Ortodoncia.vue
+++ src/views/ortodoncia/Ortodoncia.vue
@@ -1,207 +1,423 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-md-4" id="products-style" >
+      <div class="col-md-6" id="products-style">
         <div class="card">
           <img
             class="card-img-top"
             src="../../assets/ortodoncia/1-1.jpg"
             alt="Card image cap"
           />
+        </div>
+        <div class="card">
+          <div class="card-header"></div>
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">6b-01-001</th>
+                  <th scope="col">6b-01-003</th>
+                  <th scope="col">6b-01-005</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>Pinza Aderer</th>
+                  <th>Pinza Angle N°1</th>
+                  <th>Pinza Angle N°2</th>
+                </tr>
+                <tr>
+                  <th>$ 11.50</th>
+                  <th>$ 11.50</th>
+                  <th>$ 11.50</th>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
-      <div class="col-md-4" id="products-style">
+      <div class="col-md-6" id="products-style">
         <div class="card">
           <img
             class="card-img-top"
             src="../../assets/ortodoncia/1-2.jpg"
             alt="Card image cap"
           />
+        </div>
+        <div class="card">
+          <div class="card-header"></div>
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">6b-01-006</th>
+                  <th scope="col">6b-01-007</th>
+                  <th scope="col">6b-01-009</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>inza Angle N°3</th>
+                  <th>Pinza Adams</th>
+                  <th>Pinza Jhonson</th>
+                </tr>
+                <tr>
+                  <th>$ 11.50</th>
+                  <th>$ 11.50</th>
+                  <th>$ 11.50</th>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
-      <div class="col-md-4" id="products-style">
+      <div class="col-md-6" id="products-style">
         <div class="card">
           <img
             class="card-img-top"
             src="../../assets/ortodoncia/2-1.jpg"
             alt="Card image cap"
           />
+        </div>
+        <div class="card">
+          <div class="card-header"></div>
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">6b-01-010</th>
+                  <th scope="col">6b-01-011</th>
+                  <th scope="col">6b-01-012</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>Pinza Reynolds</th>
+                  <th>Pinza How Recta</th>
+                  <th>Pinza How Curva</th>
+                </tr>
+                <tr>
+                  <th>$ 11.50</th>
+                  <th>$ 11.50</th>
+                  <th>$ 11.50</th>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
-      <div class="col-md-4" id="products-style">
+      <div class="col-md-6" id="products-style">
         <div class="card">
           <img
             class="card-img-top"
             src="../../assets/ortodoncia/2-2.jpg"
             alt="Card image cap"
           />
+        </div>
+        <div class="card">
+          <div class="card-header"></div>
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">6b-01-013</th>
+                  <th scope="col">6b-01-014</th>
+                  <th scope="col">6b-01-015</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>Pinza de la Rosa Concava (1)</th>
+                  <th>Pinza de la Rosa Convexa (2)</th>
+                  <th>Pinza de la Rosa Concava (3)</th>
+                </tr>
+                <tr>
+                  <th>$ 11.50</th>
+                  <th>$ 11.50</th>
+                  <th>$ 11.50</th>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
-      <div class="col-md-4" id="products-style">
+      <div class="col-md-6" id="products-style">
         <div class="card">
           <img
             class="card-img-top"
             src="../../assets/ortodoncia/3-1.jpg"
             alt="Card image cap"
           />
+        </div>
+        <div class="card">
+          <div class="card-header"></div>
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">6b-01-016</th>
+                  <th scope="col">6b-01-017</th>
+                  <th scope="col">6b-01-018</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>Pinza Nance</th>
+                  <th>Pinza Tweed Loops</th>
+                  <th>Pinza Bending (Nance)</th>
+                </tr>
+                <tr>
+                  <th>$ 11.50</th>
+                  <th>$ 11.50</th>
+                  <th>$ 11.50</th>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
-      <div class="col-md-4" id="products-style">
+      <div class="col-md-6" id="products-style">
         <div class="card">
           <img
             class="card-img-top"
             src="../../assets/ortodoncia/3-2.jpg"
             alt="Card image cap"
           />
+        </div>
+        <div class="card">
+          <div class="card-header"></div>
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">6b-01-019</th>
+                  <th scope="col">6b-01-020</th>
+                  <th scope="col">6b-01-022</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>Pinza Aderer</th>
+                  <th>Pinza Oliver Jones</th>
+                  <th>Pinza Crown And Shell</th>
+                </tr>
+                <tr>
+                  <th>$ 11.50</th>
+                  <th>$ 11.50</th>
+                  <th>$ 11.50</th>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
-      <div class="col-md-4" id="products-style">
+      <div class="col-md-6" id="products-style">
         <div class="card">
           <img
             class="card-img-top"
             src="../../assets/ortodoncia/4-1.jpg"
             alt="Card image cap"
           />
+        </div>
+        <div class="card">
+          <div class="card-header"></div>
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">6b-01-001</th>
+                  <th scope="col">6b-01-003</th>
+                  <th scope="col">6b-01-005</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>Pinza Aderer</th>
+                  <th>Pinza Angle N°1</th>
+                  <th>Pinza Angle N°2</th>
+                </tr>
+                <tr>
+                  <th>$ 11.50</th>
+                  <th>$ 11.50</th>
+                  <th>$ 11.50</th>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
-      <div class="col-md-4" id="products-style">
+      <div class="col-md-6" id="products-style">
         <div class="card">
           <img
             class="card-img-top"
             src="../../assets/ortodoncia/4-2.jpg"
             alt="Card image cap"
           />
+        </div>
+        <div class="card">
+          <div class="card-header"></div>
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">6b-01-001</th>
+                  <th scope="col">6b-01-003</th>
+                  <th scope="col">6b-01-005</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>Pinza Aderer</th>
+                  <th>Pinza Angle N°1</th>
+                  <th>Pinza Angle N°2</th>
+                </tr>
+                <tr>
+                  <th>$ 11.50</th>
+                  <th>$ 11.50</th>
+                  <th>$ 11.50</th>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
-      <div class="col-md-4" id="products-style">
+      <div class="col-md-6" id="products-style">
         <div class="card">
           <img
             class="card-img-top"
             src="../../assets/ortodoncia/5-1.jpg"
             alt="Card image cap"
           />
+        </div>
+        <div class="card">
+          <div class="card-header"></div>
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">6b-01-001</th>
+                  <th scope="col">6b-01-003</th>
+                  <th scope="col">6b-01-005</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>Pinza Aderer</th>
+                  <th>Pinza Angle N°1</th>
+                  <th>Pinza Angle N°2</th>
+                </tr>
+                <tr>
+                  <th>$ 11.50</th>
+                  <th>$ 11.50</th>
+                  <th>$ 11.50</th>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
-      <div class="col-md-4" id="products-style">
+      <div class="col-md-6" id="products-style">
         <div class="card">
           <img
             class="card-img-top"
             src="../../assets/ortodoncia/5-2.jpg"
             alt="Card image cap"
           />
+        </div>
+        <div class="card">
+          <div class="card-header"></div>
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">6b-01-001</th>
+                  <th scope="col">6b-01-003</th>
+                  <th scope="col">6b-01-005</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>Pinza Aderer</th>
+                  <th>Pinza Angle N°1</th>
+                  <th>Pinza Angle N°2</th>
+                </tr>
+                <tr>
+                  <th>$ 11.50</th>
+                  <th>$ 11.50</th>
+                  <th>$ 11.50</th>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
-      <div class="col-md-4" id="products-style">
+      <div class="col-md-6" id="products-style">
         <div class="card">
           <img
             class="card-img-top"
             src="../../assets/ortodoncia/6-1.jpg"
             alt="Card image cap"
           />
+        </div>
+        <div class="card">
+          <div class="card-header"></div>
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">6b-01-001</th>
+                  <th scope="col">6b-01-003</th>
+                  <th scope="col">6b-01-005</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>Pinza Aderer</th>
+                  <th>Pinza Angle N°1</th>
+                  <th>Pinza Angle N°2</th>
+                </tr>
+                <tr>
+                  <th>$ 11.50</th>
+                  <th>$ 11.50</th>
+                  <th>$ 11.50</th>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
-      <div class="col-md-4" id="products-style">
+      <div class="col-md-6" id="products-style">
         <div class="card">
           <img
             class="card-img-top"
             src="../../assets/ortodoncia/6-2.jpg"
             alt="Card image cap"
           />
+        </div>
+        <div class="card">
+          <div class="card-header"></div>
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">6b-01-001</th>
+                  <th scope="col">6b-01-003</th>
+                  <th scope="col">6b-01-005</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>Pinza Aderer</th>
+                  <th>Pinza Angle N°1</th>
+                  <th>Pinza Angle N°2</th>
+                </tr>
+                <tr>
+                  <th>$ 11.50</th>
+                  <th>$ 11.50</th>
+                  <th>$ 11.50</th>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
